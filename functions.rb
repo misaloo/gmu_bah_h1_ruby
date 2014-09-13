cb728325c4c51091af022bb370ac5a40ee5e3d56
Lisa Lowe <lisa.s.lowe@gmail.com>
2014-09-13 13:27:14 -0400

    hw updates

--- functions.rb
+++ functions.rb
@@ -7,9 +7,9 @@ end
 # Letters should be lowercase before counting
 # For example, histogram('Hello') returns {'h'=>1,'e'=>1,'l'=>2,'o'=>1}
 def histogram(a_string)
-  a_string.downcase
-  #remove spaces!!!!
-  a_string.chars.group_by(&:chr).map{|k, v| [k,v.size]}
+  dcase_str = a_string.downcase
+  no_space_str = dcase_str.gsub(/\s/,"")
+  no_space_str.chars.group_by(&:chr).map{|k, v| [k,v.size]}
   #chars = a_string.split(//).uniq
   # chars.map {|x| a_string.count(x)}.join(',')
   # counts = Hash.new(0)
@@ -21,7 +21,9 @@ end
 # Be sure to only sum the numbers
 # (Hint: the is_a? method might be useful here)
 def sum_only_numbers(an_array)
-  # write your code here
+  sum = 0
+  an_array.select{|x| x.is_a?Numeric }.each{|x| sum +=x }
+  sum
 end
 
 # For i from 1 to 100, return an array.
@@ -32,11 +34,21 @@ end
 # Otherwise, the element is simply the value of i
 # For example [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', ..., 14, 'FizzBuzz', ...]
 def fizzbuzz
-  # write your code here
+  (1..100).map do |i|
+	if (i % 15).zero?
+	  'FizzBuzz'
+	elsif (i % 3).zero?
+	  'Fizz'
+	elsif (i % 5).zero?
+	  'Buzz'
+	else
+	   i
+	end
+  end
 end
 
 # Uncomment each of these to test your functions
 # puts reverse([3,6,'dog']).inspect
-puts histogram('The Quick brown fox').inspect
+# puts histogram('Hello').inspect
 # puts sum_only_numbers [4, 'foo', [ ], 27, :rain, 3.14]
-# puts fizzbuzz.join("\n")
+# puts fizzbuzz
